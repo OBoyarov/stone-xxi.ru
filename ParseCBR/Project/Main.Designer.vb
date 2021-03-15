80b5ce3b23deda12b72739eb473ba144b8122fd1
OBoyarov <80715535+OBoyarov@users.noreply.github.com>
2021-03-16 00:01:02 +0400

    Add files via upload

---
+++ ParseCBR/Project/Main.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ReqDate = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Value = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Valute = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ReqDate
+        '
+        Me.ReqDate.Location = New System.Drawing.Point(12, 25)
+        Me.ReqDate.Name = "ReqDate"
+        Me.ReqDate.Size = New System.Drawing.Size(137, 20)
+        Me.ReqDate.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Дата:"
+        '
+        'Value
+        '
+        Me.Value.Location = New System.Drawing.Point(522, 25)
+        Me.Value.Name = "Value"
+        Me.Value.ReadOnly = True
+        Me.Value.Size = New System.Drawing.Size(182, 20)
+        Me.Value.TabIndex = 2
+        Me.Value.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(519, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Курс:"
+        '
+        'Valute
+        '
+        Me.Valute.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.Valute.Font = New System.Drawing.Font("Lucida Console", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Valute.FormattingEnabled = True
+        Me.Valute.Location = New System.Drawing.Point(155, 25)
+        Me.Valute.Name = "Valute"
+        Me.Valute.Size = New System.Drawing.Size(361, 20)
+        Me.Valute.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(152, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Валюта:"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(717, 162)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Valute)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Value)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ReqDate)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ParseCBR"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ReqDate As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Value As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Valute As ComboBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ ParseCBR/Project/Main.vb
@@ -0,0 +1,103 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Main
+
+    Private ConnectionString As String = "Data Source=Inc;Database=db;Integrated Security=False;User ID=user;Password=password;Connect Timeout=30;Encrypt=False;"
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GetValDate(Now)
+        Dim DT As DataTable = Execute("exec [p_get_valutes]", "DataTable")
+        Valute.DisplayMember = "Valute"
+        Valute.DataSource = DT
+    End Sub
+    Private Sub GetValDate(ByVal DateStr As Date)
+        If Not Execute("select cast([dbo].[f_existsday] ('" & DateStr.ToString("MM/dd/yyyy") & "') as bit)", "Scalar") Then
+            Dim ClsCBR As New ClsCBR
+            Dim DTCBR As DataTable = ClsCBR.GetValCurs(DateStr.ToString("dd.MM.yyyy"))
+            If DTCBR.Rows.Count > 0 Then
+                Dim BulkCopy As SqlBulkCopy = New SqlBulkCopy(ConnectionString)
+                BulkCopy.DestinationTableName = "[cbr_data]"
+                BulkCopy.BulkCopyTimeout = 10000
+                BulkCopy.WriteToServer(DTCBR)
+            End If
+        End If
+    End Sub
+    Private Function Execute(ByVal query As String, ByVal type As String) As Object
+        Select Case type
+            Case "Scalar"
+                Using Connect As New SqlConnection(ConnectionString)
+                    Connect.Open()
+                    Using Command As New SqlCommand(query, Connect)
+                        Try
+                            Return Command.ExecuteScalar
+                        Catch ex As Exception
+                            MessageBox.Show(ex.Message & vbCrLf &
+                                            "Событие: " & Reflection.MethodBase.GetCurrentMethod().Name & vbCrLf &
+                                            "Время: " & Now.ToLocalTime,
+                                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        End Try
+                    End Using
+                End Using
+            Case "DataTable"
+                Using Connect As New SqlConnection(ConnectionString)
+                    Connect.Open()
+                    Using Command As New SqlCommand(query, Connect)
+                        Try
+                            Command.ExecuteNonQuery()
+                            Dim DA As SqlDataAdapter = New SqlDataAdapter(Command)
+                            Dim DT As DataTable = New DataTable
+                            DA.Fill(DT)
+                            Return DT
+                        Catch ex As Exception
+                            MessageBox.Show(ex.Message & vbCrLf &
+                                            "Событие: " & Reflection.MethodBase.GetCurrentMethod().Name & vbCrLf &
+                                            "Время: " & Now.ToLocalTime,
+                                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        End Try
+                    End Using
+                End Using
+            Case Else
+                Using Connect As New SqlConnection(ConnectionString)
+                    Connect.Open()
+                    Using Command As New SqlCommand(query, Connect)
+                        Try
+                            Command.ExecuteNonQuery()
+                            Return True
+                        Catch ex As Exception
+                            MessageBox.Show(ex.Message & vbCrLf &
+                                            "Событие: " & Reflection.MethodBase.GetCurrentMethod().Name & vbCrLf &
+                                            "Время: " & Now.ToLocalTime,
+                                            "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                        End Try
+                    End Using
+                End Using
+        End Select
+        Return Nothing
+    End Function
+
+    Private Sub Valute_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Valute.SelectedIndexChanged, ReqDate.ValueChanged
+        Dim valutetext As String = Valute.Text
+        Dim valutestr = valutetext.Split(New Char() {"[", "]"})(1)
+        Dim reqdatestr As String = ReqDate.Value.ToString("MM/dd/yyyy")
+        SetCurrentValue(reqdatestr, valutestr)
+    End Sub
+    Private Sub SetCurrentValue(ByVal reqdatestr As String, ByVal valutestr As String)
+        Dim currvalue As String = GetCurrentRate(reqdatestr, valutestr)
+        If currvalue.Length > 0 Then
+            Value.Text = currvalue
+        Else
+            Value.Text = "Загрузка данных..."
+            Update()
+            GetValDate(ReqDate.Value)
+            currvalue = GetCurrentRate(reqdatestr, valutestr)
+            If currvalue.Length > 0 Then
+                Value.Text = currvalue
+            Else
+                Value.Text = "Не удалось получить данные!"
+            End If
+        End If
+    End Sub
+    Private Function GetCurrentRate(ByVal reqdatestr As String, ByVal valutestr As String) As String
+        Return Execute("select [dbo].[f_get_valute] ('" & reqdatestr & "','" & valutestr & "')", "Scalar").ToString
+    End Function
+End Class
