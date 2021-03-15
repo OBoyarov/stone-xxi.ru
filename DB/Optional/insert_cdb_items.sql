fe87675cb5d055671c25d99558f9318e7677484f
OBoyarov <80715535+OBoyarov@users.noreply.github.com>
2021-03-15 23:56:51 +0400

    Create insert_cdb_items.sql

---
+++ DB/Optional/insert_cdb_items.sql
@@ -0,0 +1,120 @@
+USE [db]
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Австралийский доллар', N'Australian Dollar', 1, N'R01010    ', 36, N'AUD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Австрийский шиллинг', N'Austrian Shilling', 1000, N'R01015    ', 40, N'ATS')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Азербайджанский манат', N'Azerbaijan Manat', 1, N'R01020    ', 944, N'AZN')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Фунт стерлингов Соединенного королевства', N'British Pound Sterling', 1, N'R01035    ', 826, N'GBP')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Ангольская новая кванза', N'Angolan new Kwanza', 100000, N'R01040    ', 24, N'AON')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Армянский драм', N'Armenia Dram', 1000, N'R01060    ', 51, N'AMD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Белорусский рубль', N'Belarussian Ruble', 1, N'R01090    ', 933, N'BYN')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Бельгийский франк', N'Belgium Franc', 1000, N'R01095    ', 56, N'BEF')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Болгарский лев', N'Bulgarian lev', 1, N'R01100    ', 975, N'BGN')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Бразильский реал', N'Brazil Real', 1, N'R01115    ', 986, N'BRL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Венгерский форинт', N'Hungarian Forint', 100, N'R01135    ', 348, N'HUF')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Гонконгский доллар', N'Hong Kong Dollar', 10, N'R01200    ', 344, N'HKD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Греческая драхма', N'Greek Drachma', 10000, N'R01205    ', 300, N'GRD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Датская крона', N'Danish Krone', 10, N'R01215    ', 208, N'DKK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Доллар США', N'US Dollar', 1, N'R01235    ', 840, N'USD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Евро', N'Euro', 1, N'R01239    ', 978, N'EUR')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Индийская рупия', N'Indian Rupee', 100, N'R01270    ', 356, N'INR')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Ирландский фунт', N'Irish Pound', 100, N'R01305    ', 372, N'IEP')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Исландская крона', N'Iceland Krona', 10000, N'R01310    ', 352, N'ISK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Испанская песета', N'Spanish Peseta', 10000, N'R01315    ', 724, N'ESP')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Итальянская лира', N'Italian Lira', 100000, N'R01325    ', 380, N'ITL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Казахстанский тенге', N'Kazakhstan Tenge', 100, N'R01335    ', 398, N'KZT')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Канадский доллар', N'Canadian Dollar', 1, N'R01350    ', 124, N'CAD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Киргизский сом', N'Kyrgyzstan Som', 100, N'R01370    ', 417, N'KGS')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Китайский юань', N'China Yuan', 10, N'R01375    ', 156, N'CNY')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Кувейтский динар', N'Kuwaiti Dinar', 10, N'R01390    ', 414, N'KWD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Латвийский лат', N'Latvian Lat', 1, N'R01405    ', 428, N'LVL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Ливанский фунт', N'Lebanese Pound', 100000, N'R01420    ', 422, N'LBP')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Литовский лит', N'Lithuanian Lita', 1, N'R01435    ', 440, N'LTL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Молдавский лей', N'Moldova Lei', 10, N'R01500    ', 498, N'MDL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Немецкая марка', N'Deutsche Mark', 1, N'R01510    ', 276, N'DEM')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Немецкая марка', N'Deutsche Mark', 100, N'R01510    ', 280, N'DEM')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Нидерландский гульден', N'Netherlands Gulden', 100, N'R01523    ', 528, N'NLG')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Норвежская крона', N'Norwegian Krone', 10, N'R01535    ', 578, N'NOK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Польский злотый', N'Polish Zloty', 1, N'R01565    ', 985, N'PLN')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Португальский эскудо', N'Portuguese Escudo', 10000, N'R01570    ', 620, N'PTE')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Румынский лей', N'Romanian Leu', 10000, N'R01585    ', 642, N'ROL')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Румынский лей', N'Romanian Leu', 10, N'R01585    ', 946, N'RON')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'СДР (специальные права заимствования)', N'SDR', 1, N'R01589    ', 960, N'XDR')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Сингапурский доллар', N'Singapore Dollar', 1, N'R01625    ', 702, N'SGD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Суринамский доллар', N'Surinam Dollar', 1, N'R01665    ', 968, N'SRD')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Таджикский сомони', N'Tajikistan Ruble', 10, N'R01670    ', 972, N'TJS')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Таджикский рубл', N'Tajikistan Ruble', 10, N'R01670    ', 762, N'TJR')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Турецкая лира', N'Turkish Lira', 1, N'R01700    ', 949, N'TRY')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Туркменский манат', N'Turkmenistan Manat', 10000, N'R01710    ', 795, N'TMM')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Новый туркменский манат', N'New Turkmenistan Manat', 1, N'R01710    ', 934, N'TMT')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Узбекский сум', N'Uzbekistan Sum', 1000, N'R01717    ', 860, N'UZS')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Украинская гривна', N'Ukrainian Hryvnia', 10, N'R01720    ', 980, N'UAH')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Финляндская марка', N'Finnish Marka', 100, N'R01740    ', 246, N'FIM')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Французский франк', N'French Franc', 1000, N'R01750    ', 250, N'FRF')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Чешская крона', N'Czech Koruna', 10, N'R01760    ', 203, N'CZK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Шведская крона', N'Swedish Krona', 10, N'R01770    ', 752, N'SEK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Швейцарский франк', N'Swiss Franc', 1, N'R01775    ', 756, N'CHF')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'ЭКЮ', N'ECU', 1, N'R01790    ', 954, N'XEU')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Эстонская крона', N'Estonian Kroon', 10, N'R01795    ', 233, N'EEK')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Югославский новый динар', N'Yugoslavian Dinar', 1, N'R01804    ', 890, N'YUN')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Южноафриканский рэнд', N'S.African Rand', 10, N'R01810    ', 710, N'ZAR')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Вон Республики Корея', N'South Korean Won', 1000, N'R01815    ', 410, N'KRW')
+GO
+INSERT [dbo].[cbr_items] ([Name], [EngName], [Nominal], [ParentCode], [ISO_Num_Code], [ISO_Char_Code]) VALUES (N'Японская иена', N'Japanese Yen', 100, N'R01820    ', 392, N'JPY')
+GO
